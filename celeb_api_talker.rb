b6d0a3639538f77d26c0249413b6d2469b085247
Kyle Hogan <kylehogan@Kyles-MacBook-Air.local>
2015-01-22 13:57:34 -0500

    client code updated

--- celeb_api_talker.rb
+++ celeb_api_talker.rb
@@ -2,47 +2,67 @@ require('httparty')
 require('pry')
 require('json')
 # require('json')
-execute = 0
-while execute === 0
-  def game
-  puts "Give me the ip address you wish to connect to."
-
-  url = gets.chomp
-  url = "http://localhost:3000/" + url
-  reply = HTTParty.get(url)
-
-  puts reply
-end
-
-  puts "Which celbrity might this be?"
 
 
-  # binding.pry
-  url_ans = gets.chomp
-  url_w_ans = "http://localhost:3000/" + url_ans
-  reply = HTTParty.get(url_w_ans)
+def celeb_game
+
+
+    def game
+      puts "Give me the ip address you wish to connect to."
+      url = gets.chomp
+      reply = HTTParty.get(url)
+      puts reply
+      return url
+    end
+
+    url = game()
 
 
 
-  if reply["correct"] == true
-    puts reply["correct"]
-    execute = 100
-  else
-    puts reply["incorrect"]
-    print "Would you like to take another guess, or move onto another server?"
-    print "'again' to try again , 'move' to move on."
-    try_again = gets.chomp
-    while try_again != "again" || try_again != "move"
-      if try_again == "again"
-        puts "Which celbrity might this be?"
-        # reply = JSON.parse(reply)
+    def game_two url
+      # binding.pry
+      puts "Which celebrity might this be?"
+      url_ans = gets.chomp
+      url_w_ans = url + url_ans
+      reply = HTTParty.get(url_w_ans)
+      p url_w_ans
 
-        # binding.pry
-        url_ans = gets.chomp
-        url_w_ans = "http://localhost:3000/" + url_ans
-        reply = HTTParty.get(url_w_ans)
+      if reply["correct"]
+        puts reply["correct"]
+        execute = 100
+      else
+        puts reply["incorrect"]
+        puts "Do you wish to quit, type 'y' for yes, 'n' for no."
+        quit = gets.chomp
+        while quit != "y" && quit != "n"
+          puts "Do you wish to quit, type 'y' for yes, 'n' for no."
+          quit = gets.chomp
+        end
+        if quit == "y"
+          exit
+        end
+        print "Would you like to take another guess, or move onto another server?"
+        print "'again' to try again , 'move' to move on."
+        try_again = gets.chomp
+        while try_again != "again" && try_again != "move"
+          print "'again' to try again , 'move' to move on."
+          try_again = gets.chomp
+        end
+
+        if try_again == "again" || try_again == "move"
+          if try_again == "move"
+            celeb_game()
+          else
+            game_two(url)
+          end
+
+        else
+          print "Would you like to take another guess, or move onto another server?"
+          print "'again' to try again , 'move' to move on."
+        end
       end
     end
-  end
+      game_two(url)
 
 end
+celeb_game()
