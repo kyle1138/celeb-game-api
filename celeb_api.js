9e0385ef938ada37001b1f9d4175a074639e3dd0
Kyle Hogan <kylehogan@Kyles-MacBook-Air.local>
2015-01-22 14:03:54 -0500

    added counter to server

--- celeb_api.js
+++ celeb_api.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express();
+var counter = 0
 
 var celeb = {"results":{
   "profession":"Actor",
@@ -11,18 +12,23 @@ var celeb = {"results":{
 
 
 app.get( '/' , function(req,res){
-
+  counter ++
+  console.log(counter)
   res.send(celeb);
 
 })
 
 app.get( '/Woody_Harrelson' , function(req,res){
+  counter ++
+  console.log(counter)
   var corReply = {"correct": "You are correct. A winner is you!"}
   res.send(corReply);
 
 })
 
 app.get( '/:wrong' , function(req,res){
+  counter ++
+  console.log(counter)
   var answer = req.params.wrong;
   var wrongReply = {"incorrect": answer + " is the wrong answer. You are WRONG!"}
   res.send(wrongReply);
